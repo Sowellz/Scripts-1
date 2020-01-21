2659b2efe984efaf1c1da55adca7b8b3b5a7cf44
Best <Sunert@users.noreply.github.com>
2020-01-21 21:40:33 +0800

    fix

--- CheckIP.js
+++ CheckIP.js
@@ -95,7 +95,7 @@
  var body = $response.body;
  var obj = JSON.parse(body);
 var title =Area_check(obj['country'])+' '+' '+City_ValidCheck(obj['regionName']);
-var subtitle =flags.get(obj['countryCode'])+ISP_ValidCheck(obj['regionName'])+obj['isp']+obj['continent'];
+var subtitle =flags.get(obj['countryCode'])+ISP_ValidCheck(obj['isp'])+obj['isp']+obj['continent'];
 var ip = obj['query'];
 var description = '服务商:'+obj['isp'] +'\n'+'DNS:'+ obj['reverse'] +'\n'+'地区:' +City_ValidCheck(obj['regionName'])+ obj['query'] +'\n' +'洲际:'+obj['continent'] +'\n'+'IP:'+'\n' +'托管:'+ obj['hosting'];
  $done({title, subtitle, ip, description});
