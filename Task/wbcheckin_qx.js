3863dcb2c2a148f5e3941211b4bf8ca1964ee6fd
Best <Sunert@users.noreply.github.com>
2020-01-25 00:03:38 +0800

    add

--- Task/wbcheckin_qx.js
+++ Task/wbcheckin_qx.js
@@ -11,9 +11,9 @@ MITM = weibo.com
 var accounts = [
     // ["IU", "100808d4151ccebfbae55e8f7c0f68f6d18e4d"],
     // ["S    // ["SWITCH", "1008084239f063a3d4fb9d38a0182be6e39e76"],
-    // ["林允儿", "1008080c5fb650788fe5c7577f0b6ec4a34038"],
+    ["林允儿", "1008080c5fb650788fe5c7577f0b6ec4a34038"],
     // ["泰妍", "100808377e60b6bf5ffc9cdc603cc93b75c663"],
-    // ["Apple", "1008089f6290f4436e5a2351f12e03b6433c3c"]
+    ["Apple", "1008089f6290f4436e5a2351f12e03b6433c3c"]
 a8941058aaf4df5147042ce104568da"],
 
 ]
