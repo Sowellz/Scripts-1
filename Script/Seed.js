2572f1d93d42e013ea114c5624121d2f657a59eb
Best <Sunert@users.noreply.github.com>
2020-03-10 20:14:39 +0800

    removed other

--- Script/Seed.js
+++
@@ -1,6 +0,0 @@
-let obj = 
-[{"uuid":"7B501D7B-7CD8-45D2-840A-4D5409F537A2","UserName":"wvyjj700","Password":null,"Email":null,"Tel":null,"DueTime":"/Date(1585515200700)/","RegistrationTime":"/Date(1685515200700)/","ChannelState":0,"startTime":"/Date(1985515200700)/","StopTime":"/Date(1985515200700)/","PackageName":"三小时免费","EnPackageName":"Three hours","source":"SEEDVPN208","isRecovery":1,"currentTime":"/Date(1985515200700)/"}]
-
-$done({body: obj});
-
-
