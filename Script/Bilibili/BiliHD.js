6b7360a8e9c9219afcc1a763a16a19682c84fd77
Best <Sunert@users.noreply.github.com>
2020-03-16 17:51:51 +0800

    精简B站脚本

--- Script/Bilibili/BiliHD.js
+++ Script/Bilibili/BiliHD.js
@@ -2,7 +2,7 @@
 bilibili fan drama open 1080P+
 
 QX:
-^https?:\/\/api\.bilibili\.com\/(pgc\/player\/api\/playurl|x/vip/web/user/|x/web-interface/nav\?) url script-response-body Bilivip.js
+https:\/\/ap(p|i)\.bilibili\.com\/((pgc\/player\/api\/playurl)|(x\/v2\/account\/myinfo\?)) url script-response-body BiliHD.js
 
 QX MITM = api.bilibili.com
 */
@@ -11,9 +11,7 @@ var body = $response.body;
 var url = $request.url;
 
 const path1 = '/pgc/player/api/playurl';
-const path2 = "/vip/web/user/";
-const path3 = "/web-interface/nav\?";
-
+const path2 = "/account/myinfo\?";
 if (url.indexOf(path1) != -1) {
     let obj = JSON.parse(body);   
     obj["has_paid"] = true;
@@ -22,30 +20,13 @@ if (url.indexOf(path1) != -1) {
 	obj.vip_status = 1;
  	body = JSON.stringify(obj);  
  };
-
 if (url.indexOf(path2) != -1){
 let obj = JSON.parse(body);
- obj["data"]["vip"]["vip_status"] = 1;
-  obj.data["vip"]["vip_type"] = 2;
-  obj.data["vip"]["vip_pay_type"] = 1;
-  obj.data["vip"]["vip_due_date"]=1669824160000;
-   obj["data"]["vip"]["theme_type"] = 2;
-   obj.data["pay"]["couponBalance"] =10000;
-   obj.data["point"]["balance"]= 8888;
-  obj.data["music"] = 1000;
-body=JSON.stringify(obj);
+   obj["data"]["vip"]["type"] = 2;
+   obj["data"]["vip"]["status"] = 1;
+   obj["data"]["vip"]["vip_pay_type"] = 1;
+   
+ body=JSON.stringify(obj);
 };
-if (url.indexOf(path3) != -1){
-let obj = JSON.parse(body);
-  obj.data["vipStatus"] = 1;
-  obj.data["vipType"] = 2;
-  obj.data.has_shop = true;
-  obj.data.officialVerify["type"]= 1;
-  obj.data["vip_pay_type"]= 1;
-  obj.data.vip_theme_type = 1;
-  obj.data.vipDueDate = 1669824160;
-  obj.data["level_info"]["current_level"] = 9;
-body=JSON.stringify(obj);
-};
-
-$done({body});
+ $done({body});
+ 
